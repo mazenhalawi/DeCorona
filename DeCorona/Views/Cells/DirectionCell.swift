64498786f12ab6c65b545fb59503cc7a91adb764
Mazen <mazen.halawi79@hotmail.com>
2020-12-01 08:44:49 +0200

    Update on UI
    Amendments on UI

---
+++ DeCorona/Views/Cells/DirectionCell.swift
@@ -0,0 +1,24 @@
+//
+//  DirectionCell.swift
+//  DeCorona
+//
+//  Created by Mazen on 11/30/2020.
+//  Copyright © 2020 Mazen Halawi. All rights reserved.
+//
+
+import UIKit
+
+class DirectionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
